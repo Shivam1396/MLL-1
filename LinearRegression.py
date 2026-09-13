017ac61726e0d89334a2c19849d8726cacee223a
shivam kumar <siddharth10724@gmail.com>
2026-09-13 23:49:17 +0530

    Fix import statement for matplotlib

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,4 +1,4 @@
-ewfrgthyuimport matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
